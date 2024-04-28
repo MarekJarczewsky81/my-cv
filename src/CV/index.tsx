ce5828d73ea5136a28c015255f1e4fd08bcb985c
Marek Jarczewski <marek.jarczewski81@gmail.com>
2024-04-28 21:46:04 +0200

    Edited Props,added types.ts

--- src/CV/index.tsx
+++ src/CV/index.tsx
@@ -3,13 +3,36 @@ import { Personal } from '../Personal';
 import { Details } from '../Details';
 
 import './CV.scss';
+import { CVData } from '../types';
 
 const CV = () => {
-    return  <main className='cv-main'>
-      <Header />
-      <Personal />
-      <Details />
+  const cvData: CVData = {
+    personal: {
+      photo: "https://placehold.co/400x400?text=MJ",
+      name: "Marek",
+      lastName: "Jarczewski",
+      position: "Developer",
+    },
+    details: {
+      experience: [
+        { year: 2020, description: "Lorem ipsum"},
+        { year: 2023, description: "Dolor sit amet"},
+      ],
+      education: [
+        "Lorem ipsum",
+        "Dolor sit amet",
+        "Ala ma kota"
+      ]
+    }
+  }
+
+  return (
+    <main className='cv-main'>
+      <Header data={cvData.personal} />
+      <Personal data={cvData.personal} />
+      <Details data={cvData.details} />
     </main>
+  )
 }
 
 export { CV };
